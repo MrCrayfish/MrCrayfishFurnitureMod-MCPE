355477cb4bf1a1edf3c6b8c9beb4f59264feca42
Byteandahalf <byteandahalf@gmail.com>
2015-05-04 08:13:22 -0400

    Update WoodenTableTile.cpp

--- jni/Furniture/world/tile/WoodenTableTile.cpp
+++ jni/Furniture/world/tile/WoodenTableTile.cpp
@@ -1,6 +1,6 @@
 #include "WoodenTableTile.h"
 
 WoodenTableTile::WoodenTableTile(int id) : TableTile(id, &Material::wood) {
-    primary_texture = getTextureUVCoordinateSet("planks", 0);
-    secondary_texture = getTextureUVCoordinateSet("log", 0);
+    tex = getTextureUVCoordinateSet("planks", 0);
+    secondary_tex = getTextureUVCoordinateSet("log", 0);
 }

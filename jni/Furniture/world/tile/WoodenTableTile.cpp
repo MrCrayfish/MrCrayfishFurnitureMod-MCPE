ccfc7528ecd7ab0175d8dfd5117526fe4b02c9c1
Byteandahalf <byteandahalf@gmail.com>
2015-05-04 08:15:04 -0400

    Update WoodenTableTile.cpp

--- jni/Furniture/world/tile/WoodenTableTile.cpp
+++ jni/Furniture/world/tile/WoodenTableTile.cpp
@@ -3,4 +3,8 @@
 WoodenTableTile::WoodenTableTile(int id) : TableTile(id, &Material::wood) {
     tex = getTextureUVCoordinateSet("planks", 0);
     secondary_tex = getTextureUVCoordinateSet("log", 0);
+    
+    setNameId("tileWoodTable");
+    setSoundType(Tile::SOUND_WOOD);
+    setDestroyTime(1.0F);
 }

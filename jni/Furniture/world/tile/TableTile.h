78535fa7fba57198cff8eac02e3ce53ff7166f46
Byteandahalf <byteandahalf@gmail.com>
2015-05-04 08:11:25 -0400

    Update TableTile.h

--- jni/Furniture/world/tile/TableTile.h
+++ jni/Furniture/world/tile/TableTile.h
@@ -4,13 +4,9 @@
 
 class TableTile : public FurnitureTiles {
 public:
-	static int _woodId;
-	static int _stoneId;
-
 	TableTile(int, Material const*);
 
 	virtual const TextureUVCoordinateSet& getTexture(signed char, int);
-	virtual int getResource(int, Random*);
 	virtual bool use(Player*, int, int, int);
 
 private:

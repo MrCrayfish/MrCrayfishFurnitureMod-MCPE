95296c61db7c1e80dd3a1c150fdc47f3f17bddad
Byteandahalf <byteandahalf@gmail.com>
2015-05-04 08:12:47 -0400

    Update StoneTableTile.cpp

--- jni/Furniture/world/tile/StoneTableTile.cpp
+++ jni/Furniture/world/tile/StoneTableTile.cpp
@@ -3,4 +3,8 @@
 StoneTableTile::StoneTableTile(int id) : TableTile(id, &Material::stone) {
     tex = getTextureUVCoordinateSet("stone", 0);
     secondary_tex = getTextureUVCoordinateSet("cobblestone", 0);
+    
+    setNameId("tileStoneTable");
+    setSoundType(Tile::SOUND_STONE);
+    setDestroyTime(1.5F);
 }

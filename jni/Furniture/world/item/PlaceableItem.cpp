1d973e71603328fff927c8eb9c25e742a54589d3
Byteandahalf <byteandahalf@gmail.com>
2015-05-12 15:23:42 -0400

    Crazy for that neat code

--- jni/Furniture/world/item/PlaceableItem.cpp
+++ jni/Furniture/world/item/PlaceableItem.cpp
@@ -5,8 +5,12 @@ PlaceableItem::PlaceableItem(int id, int placed) : FurnitureItem(id) {
 }
 
 bool PlaceableItem::useOn(ItemInstance* item, Player* player, int x, int y, int z, signed char side, float xx, float yy, float zz) {
-	if(player->region.getTile(x + Facing::STEP_X[side], y + Facing::STEP_Y[side], z + Facing::STEP_Z[side]).id != 0) return false;
-	player->region.setTileAndData(x + Facing::STEP_X[side], y + Facing::STEP_Y[side], z + Facing::STEP_Z[side], {placed, 0}, 3);
+	x += Facing::STEP_X[side];
+	y += Facing::STEP_Y[side];
+	z += Facing::STEP_Z[side];
+	
+	if(player->region.getTile(x, y, z).id != 0) return false;
+	player->region.setTileAndData(x, y, z, {placed, 0}, 3);
 	item->count--;
 	return true; // return true so that the player swings his arm
 }

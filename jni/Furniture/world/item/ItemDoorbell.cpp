3affb14dfd829003ef59b5d030bfb302e7b929ce
jv110 <bebezos@hotmail.com>
2015-05-15 12:16:23 -0300

    Fixed build errors
    Removed conflicting junk files

--- jni/Furniture/world/item/ItemDoorbell.cpp
+++
@@ -1,8 +0,0 @@
-#include "DoorbellItem.h"
-
-int DoorbellItem::_id = 465;
-
-DoorbellItem::DoorbellItem(int id, std::string tex) : PlaceableItem(id, DoorbellTile::_id) {
-	setNameID(tex);
-	setIcon(tex, 3);
-}
